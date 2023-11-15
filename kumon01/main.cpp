0f62895abd65329f144789cd9a3c62ba376efc37
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-11-15 18:36:07 +0900

    [update]kumon01完了

--- kumon01/main.cpp
+++ kumon01/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 
-int main(int argc, char* argv[])
-{
-	for (int i = 1; i <= 100; ++i)
-	{
+int main(int argc, char* argv[]){
+	int num;
+	for (int i = 1; i <= 100; ++i){
 		//判定処理をここに書いてください
-		 
-		//出力は以下の形式を使用してください
-		//std::cout << i << std::endl;		//数字を出力する
-		//std::cout << "hoge" << std::endl;	//文字列を出力する
+		num = i;
+		if (num % 15 == 0) {
+			std::cout << "hogepiyo" << std::endl;
+		}
+		else if (num % 5 == 0) {
+			std::cout << "piyo" << std::endl;
+		}
+		else if (num % 3 == 0) {
+			std::cout << "hoge" << std::endl;
+		}
+		else{
+			std::cout << i << std::endl;
+		}
 	}
-
     return 0;
 }
