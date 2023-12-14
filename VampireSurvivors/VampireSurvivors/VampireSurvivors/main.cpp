c7a4971042f4c87b446c240d7c7beb018be89e98
kudononoka <106291674+kudononoka@users.noreply.github.com>
2023-12-14 19:39:56 +0900

    [update]描画

--- VampireSurvivors/VampireSurvivors/VampireSurvivors/main.cpp
+++ VampireSurvivors/VampireSurvivors/VampireSurvivors/main.cpp
@@ -1,1 +1,18 @@
 #include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+    ChangeWindowMode(TRUE);
+
+    if (DxLib_Init() == -1)	
+    {
+        return -1;			
+    }
+
+    LoadGraphScreen(0, 0, L"graphics/bleach.png", FALSE);
+
+    SetDrawScreen(DX_SCREEN_BACK);
+    WaitKey();      
+    DxLib_End();    
+    return 0;
+}
